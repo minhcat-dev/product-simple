4b6b430e828246b4269ac61591106fc922f4b733
minhcat.dev <taminhcat.dev@gmail.com>
2026-03-18 16:49:27 +0700

    theme:mantis - remove unnessary content from layouts

--- resources/views/mantis/layouts/footer.blade.php
+++ resources/views/mantis/layouts/footer.blade.php
@@ -4,12 +4,13 @@
       <div class="row">
         <div class="col-sm my-1">
           <p class="m-0"
-            >Mantis &#9829; crafted by Team <a href="https://themeforest.net/user/codedthemes" target="_blank">Codedthemes</a></p
+            >MyProject &#9829; created by <a href="" target="_blank">MinhCat</a>, theme Mantis</p
           >
         </div>
         <div class="col-auto my-1">
           <ul class="list-inline footer-link mb-0">
-            <li class="list-inline-item"><a href="../index.html">Home</a></li>
+            <li class="list-inline-item"><a href="">Home</a></li>
+            <li class="list-inline-item">v1.0.0</li>
           </ul>
         </div>
       </div>
